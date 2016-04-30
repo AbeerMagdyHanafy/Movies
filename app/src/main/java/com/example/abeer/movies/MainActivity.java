45f1a1ee258f2948735fc0517ff45e282e63a65f
Abeer Magdy <abeermagdy199525@gmail.com>
2016-04-30 20:10:27 +0200

    final project

--- app/src/main/java/com/example/abeer/movies/MainActivity.java
+++ app/src/main/java/com/example/abeer/movies/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.abeer.movies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView grid_view = (GridView) findViewById(R.id.grid_view);
+        grid_view.setAdapter(new ImageAdapter(this));
+
     }
 }
